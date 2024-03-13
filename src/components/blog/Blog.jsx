f418a015a80526d16a975c74e5192a21e5533731
abunayeem90 <naeemahmmed4@gmail.com>
2024-03-13 15:02:56 +0600

    adde blogs and blog jsx files

---
+++ src/components/blog/Blog.jsx
@@ -0,0 +1,15 @@
+import propTypes from 'prop-types';
+
+const Blog = (blog) => {
+    console.log(blog)
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+Blog.propTypes = {
+    blog: propTypes.object.isRequired
+}
+export default Blog;
